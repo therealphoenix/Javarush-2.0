658a3cd8c5f7680d2c6acff04976398f1fbee116
therealphoenix <fennya@gmail.com>
2017-04-27 12:13:45 +0300

    level 2

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0212/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task02.task0212;
+
+/* 
+Великолепная восьмерка
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat1 = new Cat();
+        Cat cat2;
+        Cat cat3 = new Cat();
+        Cat cat4;
+        Cat cat5 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat9 = new Cat();
+        Cat cat11 = new Cat();
+        Cat cat13 = new Cat();
+        Cat cat15 = new Cat();
+        //напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
